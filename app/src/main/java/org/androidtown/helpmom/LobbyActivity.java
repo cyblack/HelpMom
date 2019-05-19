3274c13f2181c8969d2df9d13b954b4cd57d2767
rpt5366 <43984143+rpt5366@users.noreply.github.com>
2019-05-19 20:23:42 +0900

    5/19 (일) 8:19 Commit 1. LobbyActivity: XML 구성. (타이틀, 유저정보 텍스트뷰, 방생성/방join 버튼2개, 참여한방 목록 보여주는 listView) 2. activity_create_room.xml : PW입력용 editText의 inputType="numberPW"로 세팅.

--- app/src/main/java/org/androidtown/helpmom/LobbyActivity.java
+++ app/src/main/java/org/androidtown/helpmom/LobbyActivity.java
@@ -2,12 +2,43 @@ package org.androidtown.helpmom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LobbyActivity extends AppCompatActivity {
+
+    ListView listView_joinedRoom;
+    Button btn_createRoom, btn_joinRoom, btn_logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        // get Views
+        btn_createRoom = (Button) findViewById(R.id.btn_createRoom);
+        btn_joinRoom = (Button) findViewById(R.id.btn_joinRoom);
+        btn_logOut = (Button) findViewById(R.id.btn_logout_lobby);
+        listView_joinedRoom = (ListView) findViewById(R.id.listView_roomList);
+
+        // 참가된 방 리스트 data.
+        List<String> joinedRoomList = new ArrayList<>();
+        joinedRoomList.add("A방");
+        joinedRoomList.add("B방");
+
+
+        // arrayAdaper로 String을 listView에 바인딩, data를 listView에 display함.
+        ArrayAdapter<String> arrayAdapter;
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, joinedRoomList);
+        listView_joinedRoom.setAdapter(arrayAdapter);
+
+
+
+
+
     }
 }
