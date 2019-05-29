860e6dc1e2992bb2b918d245fb9097053007b03e
easternpillar <roh941129@gmail.com>
2019-05-30 02:17:40 +0900

    UI Updated 4

--- app/src/main/java/org/androidtown/helpmom/LobbyActivity.java
+++ app/src/main/java/org/androidtown/helpmom/LobbyActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -22,8 +23,8 @@ public class LobbyActivity extends AppCompatActivity {
 //    TextView id,nickname;
     String _id,leader;
 
-    //List<String> joinedRoomList;
-    //ArrayAdapter<String> arrayAdapter;
+
+    private RoomListAdapter roomAdapter;
 
     private ListView listView_joinedRoom;
     private RoomListAdapter adapter;
@@ -44,7 +45,6 @@ public class LobbyActivity extends AppCompatActivity {
         hashMap=(HashMap<String,String>)intent.getSerializableExtra("hashMap");
         roomMaker=(HashMap<String,String>)intent.getSerializableExtra("roomMaker");
 
-
         //onRequestRoomList();
 
         setTitle("로비");
@@ -53,8 +53,16 @@ public class LobbyActivity extends AppCompatActivity {
         btn_joinRoom = findViewById(R.id.btn_joinRoom);
         listView_joinedRoom = findViewById(R.id.listView_roomList);
         joinedRoomList = new ArrayList<Room>();
-        adapter = new RoomListAdapter(getApplicationContext(),joinedRoomList,_id);
-        listView_joinedRoom.setAdapter(adapter);
+        roomAdapter=new RoomListAdapter(LobbyActivity.this, joinedRoomList, _id);
+        Room room1=new Room("동주네 집","장동주");
+        Room room2=new Room("형섭이네 집","송형섭");
+        Room room3=new Room("원식이네 집","신원식");
+        joinedRoomList.add(room1);
+        joinedRoomList.add(room2);
+        joinedRoomList.add(room3);
+
+        //onRequestRoomList();
+        listView_joinedRoom.setAdapter(roomAdapter);
 //        id = findViewById(R.id.txtView_lobby_userID);
 //        nickname = findViewById(R.id.txtView_lobby_userNickName);
 
@@ -69,7 +77,7 @@ public class LobbyActivity extends AppCompatActivity {
 
         // arrayAdaper로 String을 listView에 바인딩, data를 listView에 display함.
 
-        //onRequestRoomList();
+
 
         listView_joinedRoom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
